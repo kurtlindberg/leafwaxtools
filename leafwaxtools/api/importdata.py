d83a80e6b985c778de975315be4a60812a5a4edd
Kurt R. Lindberg <85883737+kurtlindberg@users.noreply.github.com>
2025-10-19 10:18:46 -0400

    Create importdata.py

---
+++ leafwaxtools/api/importdata.py
@@ -0,0 +1,13 @@
+"""
+The ImportData module is the class for importing plant wax data in commonly
+used formats (.csv, .xlsx, .txt, .lpd) into a 2D array-like object for use with
+other leafwaxtools apis
+"""
+
+import pandas as pd
+
+
+class ImportData():
+
+
+    def __init__(self):
