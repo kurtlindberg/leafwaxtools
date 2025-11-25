4e52c6e67b5f755994156aaf9b0fbb3b0d1ab56d
Kurt R. Lindberg <85883737+kurtlindberg@users.noreply.github.com>
2025-11-25 15:59:35 -0500

    Remove init for deprecated WaxData class

--- leafwaxtools/api/__init__.py
+++ leafwaxtools/api/__init__.py
@@ -2,6 +2,5 @@
 Definition of the various Classes and in which Modules they may be found.
 """
 
-from .waxdata import WaxData
 from .chain import Chain
-from .iso import Iso
+from .isotope import Isotope
