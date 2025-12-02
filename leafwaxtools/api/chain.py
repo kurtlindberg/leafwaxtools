dc90a283430b0367bf1d440ce206b9063f2dfb60
Kurt R. Lindberg <85883737+kurtlindberg@users.noreply.github.com>
2025-12-02 16:42:42 -0500

    Update imports

--- leafwaxtools/api/chain.py
+++ leafwaxtools/api/chain.py
@@ -1,20 +1,15 @@
 """
-The Chain module is the class for performing calculations using plant wax
-chain-length concentration/abundance data imported as a 2D array-like object
+The Iso module is the class for performing calculation using plant wax stable
+isotope data imported as a 2D array-like object
 """
 
-
-import pandas as pd
+# import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-from composition_stats import clr, closure, multiplicative_replacement
 import scipy.stats
-# import warnings
 # from ..utils import validate_data
 
 
-class Chain:
+class Isotope:
 
 
     def __init__(self, input_data):
@@ -22,114 +17,49 @@ class Chain:
         self.data = input_data
 
 
-    def total_conc(self, calculate_log=False, zero_total=0):
+    def iso_range(self):
 
-        total_conc = np.zeros(len(self.data[:,0]))
+        iso_range = np.zeros(len(self.data[:,0]))
 
         for row in range(0, len(self.data[:,0])):
-            total_conc[row] = np.nansum(self.data[row,:])
+            iso_range[row] = np.max(self.data[row,:]) - np.min(self.data[row,:])
 
-            if total_conc[row] == 0:
-                total_conc[row] = zero_total
-
-        match calculate_log:
-            case True:
-                total_conc = np.log(total_conc)
-            case False:
-                total_conc = total_conc
-            case _:
-                raise ValueError("'calculate_log' must either be True or False (default)")
-
-        return total_conc
+        return iso_range
 
 
-    def relative_abd(self, calculate_percent=False):
+    def iso_avg(self, chain_data):
 
-        rel_abd = np.zeros(np.shape(self.data))
+        if np.shape(self.data) != np.shape(chain_data):
+            raise ValueError("Input isotope and chain-length distribution data have the same number of rows and columns")
+
+        iso_avg = np.zeros(len(self.data[:,0]))
 
         for row in range(0, len(self.data[:,0])):
             for col in range(0, len(self.data[0,:])):
-
-                rel_abd[row,col] = self.data[row,col]/np.sum(self.data[row,:])
-                
-        match calculate_percent:
-            case True:
-                for row in range(0, len(self.data[:,0])):
-                    for col in range(0, len(self.data[0,:])):
-                        rel_abd[row,col] = rel_abd[row,col]*100
-                
-            case False:
-                rel_abd = rel_abd
-            
-            case _:
-                raise ValueError("'calculate_percent' must either be True or False (default)")
-
-        return rel_abd
-
-
-    def acl(self, chain_lengths):
-
-        if type(chain_lengths) is not type(list()):
-            raise TypeError(
-                "'chain_lengths' must be a list() type containing integers or floats; Example: [22, 24, 26, 28]"
-            )
-
-        if len(chain_lengths) < 1:
-            raise ValueError(
-                "'chain_lengths' is currently an empty list. Please make sure 'chain_lengths' contains at least 1 integer or float."
-            )
-
-        acl_numer = np.zeros(len(self.data[:,0]))
-        acl = np.zeros(len(self.data[:,0]))
+                if self.data[row,col] == np.nan:
+                    chain_data[row,col] = 0
 
         for row in range(0, len(self.data[:,0])):
             for col in range(0, len(self.data[0,:])):
+                iso_avg[row] += self.data[row,col] * chain_data[row,col]
 
-                acl_numer[row] += self.data[row,col] * chain_lengths[col]
+            iso_avg[row] = iso_avg[row]/np.sum(chain_data[row,:])
 
-            acl[row] = acl_numer[row]/np.sum(self.data[row,:])
+        return iso_avg
 
-        return acl
+    
+    def epsilon(self, epsilon_numerator=None, epsilon_denominator=None):
 
+        if epsilon_numerator is None:
+            epsilon_numerator = self.data
 
-    def cpi(self, chain_lengths, even_over_odd=True):
+        if epsilon_denominator is None:
+            epsilon_denominator = self.data
 
-        if type(chain_lengths) is not type(list()):
-            raise TypeError(
-                "'chain_lengths' must be a list() type containing integers or floats; Example: [22, 23, 24, 25, 26]"
-            )
+        epsilon = (((1000+epsilon_numerator)/(1000+epsilon_denominator))-1)*1000
 
-        if len(chain_lengths) < 1:
-            raise ValueError(
-                "'chain_lengths' is currently an empty list. Please make sure 'chain_lengths' contains at least 1 integer or float."
-            )
-
-        '''
-        EKT: use warnings to flag if even over odd order is wrong
-        '''
-
-        chain_lengths_even = [num for num in chain_lengths if num % 2 == 0]
-        chain_lengths_odd = [num for num in chain_lengths if num % 2 == 1]
-
-        data = pd.DataFrame(data=self.data, columns=(map(str, chain_lengths)))
-        data_even = np.array(data.filter(items=(map(str, chain_lengths_even))))
-        data_odd = np.array(data.filter(items=(map(str, chain_lengths_odd))))
-        cpi = np.zeros(len(self.data[:,0]))
-
-        match even_over_odd:
-            case True:
-                for row in range(0, len(self.data[:,0])):
-                    cpi[row] = (np.nansum(data_even[row,0:-1]) + np.nansum(data_even[row,1:])) / (2 * np.nansum(data_odd[row,:]))
-
-            case False:
-                for row in range(0, len(self.data[:,0])):
-                    cpi[row] = (np.nansum(data_odd[row,0:-1]) + np.nansum(data_odd[row,1:])) / (2 * np.nansum(data_even[row,:]))
-
-            case _:
-                raise ValueError("'even_over_odd' must be True (default) or False")
-
-        return cpi
-
+        return epsilon
+    
 
     def corr_rvals(self, minimum_obs=2):
 
@@ -165,82 +95,3 @@ class Chain:
                     p_vals[row,col] = np.nan
 
         return p_vals
-
-
-
-    def pca(self, chain_lengths, use_clr=True):
-
-        if type(chain_lengths) is not type(list()):
-            raise TypeError(
-                "'chain_lengths' must be a list() type containing integers or floats; Example: [22, 23, 24, 25, 26]"
-            )
-
-        if len(chain_lengths) < 1:
-            raise ValueError(
-                "'chain_lengths' is currently an empty list. Please make sure 'chain_lengths' contains at least 1 integer or float."
-            )
-
-        '''
-        # deal with missing data
-        for row in range(0, len(self.data[:,0]):
-            for col i range(0, len(self.data[0,:]):
-
-                if self.data[row,col] == np.nan:
-                    self.data[row,col] = 0
-
-            if np.sum(self.data[row,:]) == 0:
-        '''
-
-
-        match use_clr:
-            case True:
-                wax_relabd = closure(multiplicative_replacement(self.data))
-                wax_clr = clr(wax_relabd)
-                wax_data = pd.DataFrame(data=wax_clr, columns=chain_lengths)
-
-            case False:
-                wax_data = pd.DataFrame(data=self.relative_abd(), columns=chain_lengths)
-                
-            case _:
-                raise ValueError("'use_clr' must be True or False (default)")
-
-        wax_scaler = StandardScaler()
-        wax_scaler.fit(wax_data)
-        wax_data_scaled = wax_scaler.transform(wax_data)
-
-        wax_pca = PCA(n_components=len(chain_lengths))
-        wax_pca.fit_transform(wax_data_scaled)
-
-        # wax_PC_scores = pd.DataFrame(
-        #     wax_pca.fit_transform(wax_data_scaled),
-        #     columns=chain_lengths
-        # )
-        # wax_loadings = pd.DataFrame(
-        #     wax_pca.components_.T,
-        #     columns=chain_lengths,
-        #     index=wax_data.columns
-        # )
-
-        wax_ldings = wax_pca.components_
-        wax_features = wax_data.columns
-        wax_pc_values = np.arange(wax_pca.n_components_) + 1
-
-        pca_dict = {
-            "pca": wax_pca,
-            "pc_values": wax_pc_values,
-            "features": wax_features,
-            "loadings": wax_ldings
-        }
-
-        for i in range(0, len(chain_lengths)):
-
-            wax_pc = wax_pca.fit_transform(wax_data_scaled)[:,i]
-            wax_scale_pc = 1.0 / (wax_pc.max() - wax_pc.min())
-            wax_pc_score = wax_pc * wax_scale_pc
-
-            pca_dict.update({f"wax_pc{i+1}": wax_pc})
-            pca_dict.update({f"wax_scale_pc{i+1}": wax_scale_pc})
-            pca_dict.update({f"wax_pc{i+1}_score": wax_pc_score})
-
-
-        return pca_dict
