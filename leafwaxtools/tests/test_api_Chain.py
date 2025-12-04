2ba7529e852f170f7068a4944716abc910024161
Kurt R. Lindberg <85883737+kurtlindberg@users.noreply.github.com>
2025-12-04 12:28:27 -0500

    Fix block comment syntax

--- leafwaxtools/tests/test_api_Chain.py
+++ leafwaxtools/tests/test_api_Chain.py
@@ -16,68 +16,68 @@ Notes on how to test:
 5. for more details, see https://docs.pytest.org/en/stable/usage.html
 '''
 
-'''
-import os
-from pathlib import Path
-import pytest
-import pandas as pd
-import numpy as np
-from leafwaxtools import Chain, Isotope
 
-# Path to test data
-DATA_DIR = Path(__file__).parents[1].joinpath("data").resolve()
-data_path = os.path.join(DATA_DIR, "Lindberg_Arctic_terrestrial_plantwaxes.csv")
+# import os
+# from pathlib import Path
+# import pytest
+# import pandas as pd
+# import numpy as np
+# from leafwaxtools import Chain, Isotope
 
-
-class TestChainChainInit:
-    ''' Test for Chain instantiation '''
-
-    # @pytest.mark.xfail
-    def test_init_t0(self):
-        test_df = pd.read_excel(data_path)
-        test_data = Chain(np.array(test_df))
-
-        assert type(test_data.data) == pd.core.frame.DataFrame
-        #assert WaxData.data == test_data
+# # Path to test data
+# DATA_DIR = Path(__file__).parents[1].joinpath("data").resolve()
+# data_path = os.path.join(DATA_DIR, "Lindberg_Arctic_terrestrial_plantwaxes.csv")
 
 
-class TestChainChainTotal_conc:
-    ''' Test Chain.total_conc() '''
+# class TestChainChainInit:
+#     Test for Chain instantiation
 
-    # def test_total_conc_t0(self):
+#     # @pytest.mark.xfail
+#     def test_init_t0(self):
+#         test_df = pd.read_excel(data_path)
+#         test_data = Chain(np.array(test_df))
 
-
-class TestChainChainRelative_abd:
-    ''' Test Chain.relative_abd() '''
-
-    # def test_relative_abd_t0(self):
-
-
-class TestChainChainAcl:
-    ''' Test Chain.acl() '''
-
-    # def test_acl_t0(self):
+#         assert type(test_data.data) == pd.core.frame.DataFrame
+#         #assert WaxData.data == test_data
 
 
-class TestChainChainCpi:
-    ''' Test Chain.cpi() '''
+# class TestChainChainTotal_conc:
+#     Test Chain.total_conc()
 
-    # def test_cpi_t0(self):
-
-
-class TestChainChainCorr_rvals:
-    ''' Test Chain.corr_rvals() '''
-
-    # def test_corr_rvals_t0(self):
+#     # def test_total_conc_t0(self):
 
 
-class TestChainChainCorr_pvals:
-    ''' Test Chain.corr_pvals() '''
+# class TestChainChainRelative_abd:
+#     Test Chain.relative_abd()
 
-    # def test_corr_pvals_t0(self):
-'''
+#     # def test_relative_abd_t0(self):
 
-class TestChainChainPca:
-    ''' Test Chain.pca() '''
 
-    # def test_pca_t0(self):
+# class TestChainChainAcl:
+#     Test Chain.acl()
+
+#     # def test_acl_t0(self):
+
+
+# class TestChainChainCpi:
+#     Test Chain.cpi()
+
+#     # def test_cpi_t0(self):
+
+
+# class TestChainChainCorr_rvals:
+#     Test Chain.corr_rvals()
+
+#     # def test_corr_rvals_t0(self):
+
+
+# class TestChainChainCorr_pvals:
+#     Test Chain.corr_pvals()
+
+#     # def test_corr_pvals_t0(self):
+
+
+# class TestChainChainPca:
+#     Test Chain.pca()
+
+#     # def test_pca_t0(self):
