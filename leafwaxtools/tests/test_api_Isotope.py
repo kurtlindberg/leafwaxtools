bd37c88100a86ec557390468266750bb7f6f8fde
Kurt R. Lindberg <85883737+kurtlindberg@users.noreply.github.com>
2026-08-17 15:23:01 -0400

    Incorporate correlation function name changes

--- leafwaxtools/tests/test_api_Isotope.py
+++ leafwaxtools/tests/test_api_Isotope.py
@@ -112,62 +112,62 @@ class TestisotopeIsotopeValue_range:
 #     # def test_wax_to_source_t0(self):
     
 
-class TestisotopeIsotopeCorr_rvals:
+class TestisotopeIsotopeCorrelation_rvals:
     ''' Test Isotope.corr_rvals() '''
 
-    def test_corr_rvals_t0(self):
+    def test_correlation_rvals_t0(self):
         
         qpt_acid_iso_arr = np.array(qpt_acid_iso_df)
         qpt_acid_iso_obj = Isotope(qpt_acid_iso_arr)
         
-        qpt_acid_rvals = qpt_acid_iso_obj.corr_rvals(minimum_obs=2)
+        qpt_acid_rvals = qpt_acid_iso_obj.correlation_rvals(minimum_obs=2)
         
         assert np.round(qpt_acid_rvals[1,0], decimals=5) == 0.46539
             
 
-    def test_corr_rvals_t1(self):
+    def test_correlation_rvals_t1(self):
         
         qpt_acid_iso_arr = np.array(qpt_acid_iso_df)
         qpt_acid_iso_obj = Isotope(qpt_acid_iso_arr)
         
-        qpt_acid_rvals = qpt_acid_iso_obj.corr_rvals(minimum_obs=2)
+        qpt_acid_rvals = qpt_acid_iso_obj.correlation_rvals(minimum_obs=2)
         
         for col in range(len(qpt_acid_rvals[0,:])):
             assert np.round(qpt_acid_rvals[col,col], decimals=5) == 1
 
 
-    def test_corr_rvals_t2(self):
+    def test_correlation_rvals_t2(self):
         
         qpt_acid_iso_arr = np.array(qpt_acid_iso_df)
         qpt_acid_iso_obj = Isotope(qpt_acid_iso_arr)
         
-        qpt_acid_rvals = qpt_acid_iso_obj.corr_rvals(minimum_obs=2)
+        qpt_acid_rvals = qpt_acid_iso_obj.correlation_rvals(minimum_obs=2)
         
         for row in range(len(qpt_acid_rvals[:,0])):
             for col in range(len(qpt_acid_rvals[0,:])):
                 assert qpt_acid_rvals[row,col] == qpt_acid_rvals[col,row]
     
     
-class TestisotopeIsotopeCorr_pvals:
+class TestisotopeIsotopeCorrelation_pvals:
     ''' Test Chain.corr_pvals() '''
     
-    def test_corr_pvals_t0(self):
+    def test_correlation_pvals_t0(self):
         
         qpt_acid_iso_arr = np.array(qpt_acid_iso_df)
         qpt_acid_iso_obj = Isotope(qpt_acid_iso_arr)
         
-        qpt_acid_pvals = qpt_acid_iso_obj.corr_pvals(minimum_obs=2)
+        qpt_acid_pvals = qpt_acid_iso_obj.correlation_pvals(minimum_obs=2)
         
         for col in range(len(qpt_acid_pvals[0,:])):
             assert np.round(qpt_acid_pvals[col,col], decimals=5) == 0
             
             
-    def test_corr_pvals_t1(self):
+    def test_correlation_pvals_t1(self):
         
         qpt_acid_iso_arr = np.array(qpt_acid_iso_df)
         qpt_acid_iso_obj = Isotope(qpt_acid_iso_arr)
         
-        qpt_acid_pvals = qpt_acid_iso_obj.corr_pvals(minimum_obs=2)
+        qpt_acid_pvals = qpt_acid_iso_obj.correlation_pvals(minimum_obs=2)
         
         for row in range(len(qpt_acid_pvals[:,0])):
             for col in range(len(qpt_acid_pvals[0,:])):
