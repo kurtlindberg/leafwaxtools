60f6c62d9cc4953033e7de0d2b8ca1090242ec17
Kurt R. Lindberg <85883737+kurtlindberg@users.noreply.github.com>
2026-08-17 13:43:17 -0400

    Add docstrings

--- leafwaxtools/utils/validate_init.py
+++ leafwaxtools/utils/validate_init.py
@@ -2,6 +2,26 @@ import numpy as np
 
 
 def validate_data_dimensions(data):
+    """
+    Raises a TypeError if the data arg is not 2-dimensional.
+
+    Parameters
+    ----------
+    data : 2-D array-like
+        Uses the self.data (input_data) variable in the __init__ for the Chain
+        and Isotope API classes.
+
+    Raises
+    ------
+    TypeError
+        Raises and error when Numpy array-transformed 'data' is not 
+        2-dimensional.
+
+    Returns
+    -------
+    None.
+
+    """
     
     data_arr = np.array(data)
     
