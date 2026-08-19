14edf298b189d07ccddef04f78d542ccfce77864
Kurt R. Lindberg <85883737+kurtlindberg@users.noreply.github.com>
2026-08-19 14:17:59 -0400

    Update tests for new NaN type checking

--- leafwaxtools/tests/test_api_Isotope.py
+++ leafwaxtools/tests/test_api_Isotope.py
@@ -41,6 +41,18 @@ arctic_acid_iso_df = arctic_data_df[
     ]
 ]
 
+arctic_acid_chain_df = arctic_data_df[
+    [
+        'c20_fconc',
+        'c22_fconc',
+        'c24_fconc',
+        'c26_fconc',
+        'c28_fconc',
+        'c30_fconc',
+        'c32_fconc',
+    ]
+]
+
 qpt_data_df = pd.read_csv(qpt_data_path)
 qpt_acid_iso_df = qpt_data_df[
     [
@@ -53,26 +65,29 @@ qpt_acid_iso_df = qpt_data_df[
     ]
 ]
 
+qpt_acid_chain_df = qpt_data_df[
+    [
+        'c20concentration',
+        'c22concentration',
+        'c24concentration',
+        'c26concentration',
+        'c28concentration',
+        'c30concentration',
+    ]
+]
+
+
 class TestisotopeIsotopeInit:
     ''' Test Isotope instantiation '''
-
+    
     def test_init_t0(self):
         
-        arctic_acid_iso_arr = np.array(arctic_acid_iso_df)
-        arctic_acid_iso_obj = Isotope(arctic_acid_iso_arr)
-        
-        assert arctic_acid_iso_obj.data.all() == arctic_acid_iso_arr.all()
-    
-    
-    def test_init_t1(self):
-        
-        arctic_acid_iso_arr = np.array(arctic_acid_iso_df)
-        arctic_acid_iso_obj = Isotope(arctic_acid_iso_arr)
+        arctic_acid_iso_obj = Isotope(arctic_acid_iso_df)
         
         assert arctic_acid_iso_obj.data.ndim == 2
 
 
-    def test_init_t2(self):
+    def test_init_t1(self):
         
         with pytest.raises(TypeError):
             arctic_c22_ser = pd.Series(data=arctic_acid_iso_df.c22_fd2h)
@@ -85,20 +100,44 @@ class TestisotopeIsotopeValue_range:
 
     def test_value_range_t0(self):
         
-        arctic_acid_iso_arr = np.array(arctic_acid_iso_df)
-        arctic_acid_iso_obj = Isotope(arctic_acid_iso_arr)
-        
+        arctic_acid_iso_obj = Isotope(arctic_acid_iso_df)
         arctic_acid_iso_range = arctic_acid_iso_obj.value_range()
         
+        assert np.isnan(arctic_acid_iso_range[0]) == True
         assert arctic_acid_iso_range[6] == 15
         assert arctic_acid_iso_range[-2] == 14
+        assert np.isnan(arctic_acid_iso_range[-1]) == True
 
 
-# class TestisotopeIsotopeConcentration_avg:
-#     Test Isotope.concentration_avg()
+class TestisotopeIsotopeConcentration_avg:
+    ''' Test Isotope.concentration_avg() '''
 
-#     # def test_concentration_avg_t0(self):
+    def test_concentration_avg_t0(self):
+        
+        arctic_acid_iso_obj = Isotope(arctic_acid_iso_df)
+        arctic_acid_conc_avg = arctic_acid_iso_obj.concentration_avg(chain_data=arctic_acid_chain_df)
+        
+        assert np.isnan(arctic_acid_conc_avg[0]) == True
+        assert np.round(arctic_acid_conc_avg[6], decimals=3) == -216.111
+        assert arctic_acid_conc_avg[-2] == -211
+        assert np.isnan(arctic_acid_conc_avg[-1]) == True
+        
+        
 
+    def test_concentration_avg_t1(self):
+        
+        with pytest.raises(ValueError):
+            arctic_acid_iso_obj = Isotope(arctic_acid_iso_df)
+            arctic_acid_somechain_df = arctic_data_df[
+                [
+                    'c22_fconc',
+                    'c24_fconc',
+                    'c26_fconc',
+                    'c28_fconc'
+                ]
+            ]
+            arctic_acid_conc_avg = arctic_acid_iso_obj.concentration_avg(chain_data=arctic_acid_somechain_df)
+        
 
 # class TestisotopeIsotopeEpsilon:
 #     Test Isotope.epsilon()
@@ -117,9 +156,7 @@ class TestisotopeIsotopeCorrelation_rvals:
 
     def test_correlation_rvals_t0(self):
         
-        qpt_acid_iso_arr = np.array(qpt_acid_iso_df)
-        qpt_acid_iso_obj = Isotope(qpt_acid_iso_arr)
-        
+        qpt_acid_iso_obj = Isotope(qpt_acid_iso_df)
         qpt_acid_rvals = qpt_acid_iso_obj.correlation_rvals(minimum_obs=2)
         
         assert np.round(qpt_acid_rvals[1,0], decimals=5) == 0.46539
@@ -127,9 +164,7 @@ class TestisotopeIsotopeCorrelation_rvals:
 
     def test_correlation_rvals_t1(self):
         
-        qpt_acid_iso_arr = np.array(qpt_acid_iso_df)
-        qpt_acid_iso_obj = Isotope(qpt_acid_iso_arr)
-        
+        qpt_acid_iso_obj = Isotope(qpt_acid_iso_df)
         qpt_acid_rvals = qpt_acid_iso_obj.correlation_rvals(minimum_obs=2)
         
         for col in range(len(qpt_acid_rvals[0,:])):
@@ -138,9 +173,7 @@ class TestisotopeIsotopeCorrelation_rvals:
 
     def test_correlation_rvals_t2(self):
         
-        qpt_acid_iso_arr = np.array(qpt_acid_iso_df)
-        qpt_acid_iso_obj = Isotope(qpt_acid_iso_arr)
-        
+        qpt_acid_iso_obj = Isotope(qpt_acid_iso_df)
         qpt_acid_rvals = qpt_acid_iso_obj.correlation_rvals(minimum_obs=2)
         
         for row in range(len(qpt_acid_rvals[:,0])):
@@ -153,9 +186,7 @@ class TestisotopeIsotopeCorrelation_pvals:
     
     def test_correlation_pvals_t0(self):
         
-        qpt_acid_iso_arr = np.array(qpt_acid_iso_df)
-        qpt_acid_iso_obj = Isotope(qpt_acid_iso_arr)
-        
+        qpt_acid_iso_obj = Isotope(qpt_acid_iso_df)
         qpt_acid_pvals = qpt_acid_iso_obj.correlation_pvals(minimum_obs=2)
         
         for col in range(len(qpt_acid_pvals[0,:])):
@@ -164,9 +195,7 @@ class TestisotopeIsotopeCorrelation_pvals:
             
     def test_correlation_pvals_t1(self):
         
-        qpt_acid_iso_arr = np.array(qpt_acid_iso_df)
-        qpt_acid_iso_obj = Isotope(qpt_acid_iso_arr)
-        
+        qpt_acid_iso_obj = Isotope(qpt_acid_iso_df)
         qpt_acid_pvals = qpt_acid_iso_obj.correlation_pvals(minimum_obs=2)
         
         for row in range(len(qpt_acid_pvals[:,0])):
